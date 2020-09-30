6837f67526a9a4d3d685a06c9311da61ac9d7f82
derossiwang <wcsmyones@gmail.com>
2020-09-30 15:57:09 +1000

    added most basic bottom navigation layout
     --- Basic Bottom Navigation layout added---
    Three tags for the navigation menu.
    Next step is designing the basic layout for other pages

---
+++ Application/app/src/main/java/com/example/group_w01_07_3/Capsule.java
@@ -0,0 +1,46 @@
+package com.example.group_w01_07_3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Capsule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capsule);
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set page selected
+        bottomNavigationView.setSelectedItemId(R.id.capsule);
+
+        //Item Selected Listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.discover:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.capsule:
+                        return true;
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(), Account.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
---
+++ Application/app/src/main/java/com/example/group_w01_07_3/Discover.java
@@ -0,0 +1,46 @@
+package com.example.group_w01_07_3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Discover extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover);
+
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set page selected
+        bottomNavigationView.setSelectedItemId(R.id.discover);
+
+        //Item Selected Listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.discover:
+                        return true;
+                    case R.id.capsule:
+                        startActivity(new Intent(getApplicationContext(), Capsule.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(), Account.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
