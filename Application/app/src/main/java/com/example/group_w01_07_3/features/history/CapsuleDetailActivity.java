0eaeb8a478c5f956ed4983b248d3caef962e6ae3
derossiwang <wcsmyones@gmail.com>
2020-10-21 00:22:32 +1100

    detail capsule class created

---
+++ Application/app/src/main/java/com/example/group_w01_07_3/features/history/CapsuleDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.group_w01_07_3.features.history;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CapsuleDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capsule_detail);
+    }
+}
