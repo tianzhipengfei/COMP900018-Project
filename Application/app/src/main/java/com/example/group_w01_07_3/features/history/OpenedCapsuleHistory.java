db2550c3b1ab49c95037ac41c5f055b3fed7ed7d
derossiwang <wcsmyones@gmail.com>
2020-10-28 02:09:28 +1100

    Update OpenedCapsuleHistory.java

--- Application/app/src/main/java/com/example/group_w01_07_3/features/history/OpenedCapsuleHistory.java
+++ Application/app/src/main/java/com/example/group_w01_07_3/features/history/OpenedCapsuleHistory.java
@@ -184,6 +184,7 @@ public class OpenedCapsuleHistory extends AppCompatActivity implements
                             testingList.add(new OpenedCapsule("New one ADDED: 1st", "2016/12/31", "null", "null",1,"sfgdfsgsdfsdfgsdfgsdfg","wcs123455","null"));
                             testingList.add(new OpenedCapsule("New one ADDED: 2nd", "2017/12/31", "null", "https://www.tianzhipengfei.xin/static/mobile/wcs123455-1603700323.jpg",1,"sdfgsdfgdsfgfsdgdsgdsfgs","wcs123455","12345"));
                             testingList.add(new OpenedCapsule("New one ADDED: 3rd", "2018/12/31", "https://www.tianzhipengfei.xin/static/mobile/wcs123455-1603700356.jpg", "null",1,"sdfgsdgfsdgsdfgsdgsdfgds","wcs123455","12345"));
+                            testingList.add(new OpenedCapsule("New one ADDED: 4th", "2018/12/31", "null", "https://www.tianzhipengfei.xin/static/mobile/wcs123455-1603700436.jpg",1,"sdfgsdgfsdgsdfgsdgsdfgds","wcs123455","12345"));
                             openedCapsuleAdapter.notifyDataSetChanged();
                             Toast.makeText(OpenedCapsuleHistory.this, "first round refresh notified", Toast.LENGTH_SHORT).show();
                             //必须要晚一点设置complete
