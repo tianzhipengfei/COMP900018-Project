f5a0b8f2ca91988086b1a50d39680d33861de63c
Chen Fu <xm1057633189@vip.qq.com>
2020-11-03 14:18:10 +0800

    Update and add comments in CaesarCipherUtil
    Update and add comments in CaesarCipherUtil

--- Application/app/src/main/java/com/example/group_w01_07_3/util/CaesarCipherUtil.java
+++ Application/app/src/main/java/com/example/group_w01_07_3/util/CaesarCipherUtil.java
@@ -2,8 +2,17 @@ package com.example.group_w01_07_3.util;
 
 // (Modified) From: https://blog.csdn.net/theUncle/article/details/100156976?utm_medium=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param#3.DES%28Data%20Encryption%20Standard%29
 
+/**
+ * CaesarCipherUtil is used to encrypt or decrypt string
+ */
 public class CaesarCipherUtil {
 
+    /**
+     * CaesarCipherUtil encrypt
+     * @param original the string to be encrypted
+     * @param key encryption key
+     * @return encrypted string
+     */
     public static String encrypt(String original, int key) {
         char[] chars = original.toCharArray();
         StringBuffer buffer = new StringBuffer();
@@ -16,6 +25,12 @@ public class CaesarCipherUtil {
         return buffer.toString();
     }
 
+    /**
+     * CaesarCipherUtil decrypt
+     * @param encryptedData the string to be decrypted
+     * @param key decryption key
+     * @return decrypted string
+     */
     public static String decrypt(String encryptedData, int key) {
         char[] chars = encryptedData.toCharArray();
         StringBuilder sb = new StringBuilder();
