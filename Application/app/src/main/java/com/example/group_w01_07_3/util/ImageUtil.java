f31b700152ebc143752478994b2a74a9a0d32ea5
Chen Fu <xm1057633189@vip.qq.com>
2020-11-03 13:41:45 +0800

    Add comments in ImageUtil
    Add comments in ImageUtil

--- Application/app/src/main/java/com/example/group_w01_07_3/util/ImageUtil.java
+++ Application/app/src/main/java/com/example/group_w01_07_3/util/ImageUtil.java
@@ -12,6 +12,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -32,18 +33,19 @@ public class ImageUtil {
      * @param context  context
      * @param bitmap   image bitmap
      * @param filename image filename
-     * @return
+     * @return compressed image file
      */
     public static File compressImage(Context context, Bitmap bitmap, String filename) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 30, baos);
-        int options = 100;
-        while (baos.toByteArray().length / 1024 > 500) {
-            baos.reset();
-            options -= 10;
-            bitmap.compress(Bitmap.CompressFormat.JPEG, options, baos);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 30, baos); // compress 30%
+        int options = 30;
+        while (baos.toByteArray().length / 1024 > 100) {
+            // loop to determine if the compressed image is larger than 100kb, and continue to compress if larger
+            baos.reset(); // clear baos
+            options -= 5; // every time reduce by 5
+            bitmap.compress(Bitmap.CompressFormat.JPEG, options, baos); // compress options% and store the compressed data in baos
         }
-        File file = new File(context.getExternalCacheDir(), filename);
+        File file = new File(context.getExternalCacheDir(), filename); // image file
         try {
             FileOutputStream fos = new FileOutputStream(file);
             try {
@@ -70,9 +72,9 @@ public class ImageUtil {
         Request request = new Request.Builder()
                 .url(urlString)
                 .build();
-        okHttpClient.newCall(request).enqueue(new Callback() {
+        okHttpClient.newCall(request).enqueue(new Callback() { // get image bitmap from Http image url
             public void onResponse(@NotNull Call call, @NotNull Response response) {
-                InputStream inputStream = response.body().byteStream();
+                InputStream inputStream = Objects.requireNonNull(response.body()).byteStream();
                 urlBitmap = BitmapFactory.decodeStream(inputStream);
             }
 
